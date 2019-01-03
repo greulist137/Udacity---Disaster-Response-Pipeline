13a15db62a7765de288a864812b045c05042e4ef
Steven Greulich <greulich.steven@outlook.com>
2019-01-03 18:18:30 -0500

    Feat - Completion of Train_Classifier.py
    Author: Steven Greulich
    Purpose of Commit: The purpose of this commit was for the completion of the train_classifier.py document.  This file trains, predicts and evaluates the model

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,25 +1,58 @@
+# import libraries
 import sys
-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
+import nltk
+nltk.download(['punkt', 'wordnet'])
+from nltk.tokenize import word_tokenize
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import re
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
+from sklearn.neighbors import KNeighborsClassifier
+import pickle
 
 def load_data(database_filepath):
-    pass
-
+    engine = create_engine('sqlite:///DisasterResponse.db')
+    df =  pd.read_sql_table('DisasterResponse', engine)
+    X = df.message.values
+    y = df.iloc[:,5:]
+    return X, y, y.keys()
 
 def tokenize(text):
-    pass
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
 
+    return clean_tokens
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    return pipeline
 
-
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
-
+def evaluate_model(pipeline, X_test, Y_test, category_names):
+    # predict on test data
+    y_pred = pipeline.predict(X_test)
+    print(classification_report(Y_test, y_pred, target_names=Y_test.keys()))
 
 def save_model(model, model_filepath):
-    pass
-
+    # save the model to disk
+    filename = 'finalized_model.sav'
+    pickle.dump(model, open(filename, 'wb'))
 
 def main():
     if len(sys.argv) == 3:
